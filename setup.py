d4233be2798fc7fbc8c192bc27dded027b1a811c
keshavagrawal89 <keshav_agrawal89@hotmail.com>
2012-10-26 18:38:01 +0530

    version change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (2, 6):
 setup(
     name = "plivo",
     py_modules = ['plivo'],
-    version = "0.2.8",
+    version = "0.2.9",
     description = "Plivo Python library",
     author = "Plivo Team",
     author_email = "hello@plivo.com",
