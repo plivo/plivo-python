105305b927c79ad7ba4a62ffc6f8c8d7bec12a5f
Kritarth <kritarth@plivo.com>
2020-04-16 13:15:27 +0530

    add jwt generation and tests

---
+++ plivo/utils/jwt.py
@@ -0,0 +1,81 @@
+import jwt, time
+from plivo.utils.validators import *
+
+
+class AccessToken:
+    auth_id = ''
+    auth_token = ''
+    username = ''
+    valid_from = int(time.time())
+    lifetime = 86400
+    key = ''
+    grants = {}
+    uid = time.time()
+
+    @validate_args(
+        auth_id=[is_account_id()],
+        auth_token=[optional(of_type(six.text_type))],
+        username=[optional(of_type(six.text_type))],
+        valid_from=[optional(of_type(*six.integer_types))],
+        lifetime=[
+            optional(
+                all_of(
+                    of_type(*six.integer_types),
+                    check(lambda lifetime: 180 <= lifetime <= 86400,
+                          '180 < lifetime <= 86400')))
+        ],
+        valid_till=[optional(of_type(*six.integer_types))],
+    )
+    def __init__(self,
+                 auth_id,
+                 auth_token,
+                 username,
+                 valid_from=None,
+                 lifetime=None,
+                 valid_till=None,
+                 uid=None):
+        self.auth_id = auth_id
+        self.auth_token = auth_token
+        self.username = username
+        if valid_from:
+            self.valid_from = int(valid_from)
+        if lifetime:
+            self.lifetime = int(lifetime)
+            if valid_till is not None:
+                raise ValidationError("use either lifetime or valid_till")
+        elif valid_till:
+            self.lifetime = valid_till - self.valid_from
+            if self.lifetime < 0:
+                raise ValidationError(
+                    "validity expires %s seconds before it starts" %
+                    self.lifetime)
+
+        self.key = auth_token
+
+        if uid:
+            self.uid = uid
+        else:
+            self.uid = "%s-%s" % (username, self.uid)
+
+    @validate_args(
+        incoming=[optional(of_type_exact(bool))],
+        outgoing=[optional(of_type_exact(bool))],
+    )
+    def add_voice_grants(self, incoming=False, outgoing=False):
+        self.grants['voice'] = {
+            'incoming_allow': incoming,
+            'outgoing_allow': outgoing
+        }
+
+    def to_jwt(self):
+        headers = {'typ': 'JWT', 'cty': 'plivo;v=1'}
+        algorithm = 'HS256'
+        claims = {
+            'jti': self.uid,
+            'iss': self.auth_id,
+            'sub': self.username,
+            'nbf': self.valid_from,
+            'exp': self.valid_from + self.lifetime,
+            'grants': self.grants
+        }
+        return jwt.encode(claims, self.key, algorithm, headers).decode('utf-8')
---
+++ tests/resources/test_jwt.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+import plivo
+import time
+from tests.base import PlivoResourceTestCase
+from plivo.utils import jwt
+
+
+class AccessTokenTest(PlivoResourceTestCase):
+    def test_jwt_constructor(self):
+        self.assertRaisesRegex(TypeError, "", jwt.AccessToken.__init__,
+                               jwt.AccessToken, 'ADADADADADADADADADA',
+                               'qwerty')
+
+    def test_jwt_constructor_auth_id(self):
+        self.assertRaisesRegex(plivo.exceptions.ValidationError,
+                               "auth_id should match format .*",
+                               jwt.AccessToken.__init__, jwt.AccessToken,
+                               'ADADADADADADADADADA', 'qwerty', 'username')
+
+    def test_jwt_constructor_auth_id(self):
+        self.assertRaisesRegex(plivo.exceptions.ValidationError,
+                               "auth_id should match format .*",
+                               jwt.AccessToken.__init__, jwt.AccessToken,
+                               'ADADADADADADADADADA', 'qwerty', 'username')
+
+    def test_jwt_constructor_lifetime(self):
+        self.assertRaisesRegex(plivo.exceptions.ValidationError,
+                               ".* lifetime .*",
+                               jwt.AccessToken.__init__,
+                               jwt.AccessToken,
+                               'MADADADADADADADADADA',
+                               'qwerty',
+                               'username',
+                               valid_from=int(time.time()),
+                               lifetime=123)
+
+    def test_jwt_constructor_validity(self):
+        self.assertRaisesRegex(plivo.exceptions.ValidationError,
+                               "use either lifetime or valid_till",
+                               jwt.AccessToken.__init__,
+                               jwt.AccessToken,
+                               'MADADADADADADADADADA',
+                               'qwerty',
+                               'username',
+                               valid_from=int(time.time()),
+                               valid_till=int(time.time()) - 100,
+                               lifetime=1200)
+        self.assertRaisesRegex(plivo.exceptions.ValidationError,
+                               "validity expires .* seconds before it starts",
+                               jwt.AccessToken.__init__,
+                               jwt.AccessToken,
+                               'MADADADADADADADADADA',
+                               'qwerty',
+                               'username',
+                               valid_from=int(time.time()),
+                               valid_till=int(time.time()) - 100)
+
+    def test_jwt(self):
+        token = jwt.AccessToken('MADADADADADADADADADA', 'qwerty', 'username', valid_from=12121212, lifetime=300, uid='username-12345')
+        token.add_voice_grants(True, True)
+        self.assertEqual(True, token.grants['voice']['incoming_allow'])
+        self.assertNotEqual(False, token.grants['voice']['outgoing_allow'])
+        self.assertEqual('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InBsaXZvO3Y9MSJ9.eyJqdGkiOiJ1c2VybmFtZS0xMjM0NSIsImlzcyI6Ik1BREFEQURBREFEQURBREFEQURBIiwic3ViIjoidXNlcm5hbWUiLCJuYmYiOjEyMTIxMjEyLCJleHAiOjEyMTIxNTEyLCJncmFudHMiOnsidm9pY2UiOnsiaW5jb21pbmdfYWxsb3ciOnRydWUsIm91dGdvaW5nX2FsbG93Ijp0cnVlfX19.khM99-sYP2AylLo9y6bwNnJbVPjjtOMAimiFvNo7FGA', token.to_jwt())
