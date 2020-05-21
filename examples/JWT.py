dda558e5ac00e98ff2cdc3ce08c6f4308e1bda0d
Kritarth <kritarth@plivo.com>
2020-05-21 15:04:03 +0530

    example update

--- examples/JWT.py
+++ examples/JWT.py
@@ -2,13 +2,13 @@ from plivo.utils import jwt
 import time
 
 # using valid_from in epoch and lifetime in seconds
-token = jwt.AccessToken('MADADADADADADADADADA', 'qwerty', 'username', valid_from=time.time(), lifetime=300, uid='username-12345')
+token = jwt.AccessToken('{authId}', '{authToken}', '{endpointUsername}', valid_from=time.time(), lifetime=300, uid='{uid}')
 # grants(incoming:bool, outgoing:bool)
 token.add_voice_grants(True, True)
 print(token.to_jwt())
 
 
 # using valid_from and valid_till in epoch
-token = jwt.AccessToken('MADADADADADADADADADA', 'qwerty', 'username', valid_from=time.time(), valid_till=1588751222)
+token = jwt.AccessToken('{authId}', '{authToken}', '{endpointUsername}', valid_from=time.time(), valid_till=1588751222)
 token.add_voice_grants(False, True)
 print(token.to_jwt())
