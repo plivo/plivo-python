07fed1d246f37e55c865180411a531a95fdd27b3
kowshik-plivo <intern-kowshik@plivo.com>
2022-07-06 21:08:18 +0530

    VT-4406

--- plivo/resources/token.py
+++ plivo/resources/token.py
@@ -1,8 +1,4 @@
-import numbers
-from this import s
-import jwt
-import time
-import sys,json,requests
+import json,requests
 from plivo.base import (ListResponseObject, PlivoResource,
                         PlivoResourceInterface)
 from plivo.utils import to_param_dict
@@ -11,45 +7,56 @@ from plivo.utils.validators import *
 
 class Token(PlivoResourceInterface):
     @validate_args(
+        iss = [required(of_type(six.text_type))],
+        auth_token = [required(of_type(six.text_type))],
         sub = [optional(of_type(six.text_type))],
-        iss = [optional(of_type(six.text_type))],
-        nbf = [optional(of_type(numbers))],
-        exp = [optional(of_type(numbers))], 
-        per = [
-            all_of(
-                of_type(six.text_type),
-                is_in(('voice','incoming_allow', 'outgoing_allow')))
-        ],
-        incoming_allowed = [optional(of_type_exact(bool))],
-        outgoing_allowed = [optional(of_type_exact(bool))],
-        app = [optional(of_type(six.text_type))],
-        auth_id = [required(of_type(six.text_type))],
-
-
+        nbf = [required(of_type(six.text_type))],
+        exp = [required(of_type(six.text_type))],
+        incoming_allowed = [optional(of_type(bool))],
+        outgoing_allowed = [optional(of_type(bool))]
+        # app = [optional(of_type(six.text_type))]
     )
-    def create(self, iss:str, sub:str,nbf:str, exp:str, incoming_allowed:bool, outgoing_allowed:bool, app:str):
+
+    # def create(iss:str, auth_token:str, sub:str,nbf:str, exp:str, incoming_allowed:bool, outgoing_allowed:bool):
+    def create(iss:str, auth_token:str, sub:str,nbf:str, exp:str, incoming_allowed:bool, outgoing_allowed:bool, app:str):
+        flag = 0
+        if(app == ""):
+            flag = 1
         url = "https://api-qa.voice.plivodev.com/v1/Account/"+iss+"/JWT/Token"
-        payload = json.dumps({
+
+        if(flag == 1):
+            payload = json.dumps({
             "sub": sub,
             "iss": iss,
             "nbf": nbf,
             "exp": exp,
             "per": {
                 "voice": {
-                "incoming_allow": incoming_allowed,
-                "outgoing_allow": outgoing_allowed
+                "incoming_allow": False,
+                "outgoing_allow": False
+                }
+            }
+            })
+        else:
+            payload = json.dumps({
+            "sub": sub,
+            "iss": iss,
+            "nbf": nbf,
+            "exp": exp,
+            "per": {
+                "voice": {
+                "incoming_allow": False,
+                "outgoing_allow": False
                 }
             },
             "app": app
             })
         headers = {
-           'authorization': 'Basic TUFNRFZMWkpZMlpHWTVNV1UxWko6WlRKaU9EYzVNVE0xT0RBM016TXlZbVk0TlRBd1l6QTNNamMyT1dOaA==',
-           'cache-control': 'no-cache',
-           'content-type': 'application/json',
-           'postman-token': '6a4ab3a7-cb2d-a410-fc39-220af5712d23'
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic TUFNSk01TlRCS01NSkhPVFJNWkQ6TkRBeU5HTTFZelkwTTJVeFkyTmlNak16TVdGbU1qVXlNVGc0TmprNA=='
         }
-        response = requests.request("POST", url, headers=headers, data=payload)
+        print("payload", payload)
+        print("headers", headers)
+        print("url", url)
+        response = requests.request("POST", url, headers=headers,data=payload)
         return response.text
-
-
-    
