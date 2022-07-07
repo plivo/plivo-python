b6157ca5196240e87b8792bc2264edea0a107795
kowshik-plivo <107911744+kowshik-plivo@users.noreply.github.com>
2022-07-07 16:31:02 +0530

    Updated to current style

--- plivo/resources/token.py
+++ plivo/resources/token.py
@@ -1,9 +1,7 @@
-import json,requests
-from plivo.base import (ListResponseObject, PlivoResource,
-                        PlivoResourceInterface)
+import json
+from plivo.base import (ListResponseObject, PlivoResource,PlivoResourceInterface)
 from plivo.utils import to_param_dict
 from plivo.utils.validators import *
-
 
 class Token(PlivoResourceInterface):
     @validate_args(
@@ -17,45 +15,21 @@ class Token(PlivoResourceInterface):
         app = [optional(of_type(six.text_type))]
     )
 
-    def create(iss:str, auth_token:str, sub:str,nbf:str, exp:str, incoming_allowed:bool, outgoing_allowed:bool, app:str):
-        flag = 0
-        if(app == ""):
-            flag = 1
-        url = "https://api-qa.voice.plivodev.com/v1/Account/"+iss+"/JWT/Token"
-
-        if(flag == 1):
-            payload = json.dumps({
-            "sub": sub,
-            "iss": iss,
-            "nbf": nbf,
-            "exp": exp,
-            "per": {
-                "voice": {
-                "incoming_allow": False,
-                "outgoing_allow": False
-                }
-            }
-            })
+    def create( self,iss, auth_token, sub,nbf, exp, incoming_allowed, outgoing_allowed,app):
+        if(incoming_allowed == True and sub == None):
+                raise ValidationError('Subaccount is required')
         else:
             payload = json.dumps({
-            "sub": sub,
-            "iss": iss,
-            "nbf": nbf,
-            "exp": exp,
-            "per": {
+                "sub": sub,
+                "iss": iss,
+                "nbf": nbf,
+                "exp": exp,
+                "per": {
                 "voice": {
-                "incoming_allow": False,
-                "outgoing_allow": False
-                }
-            },
-            "app": app
+                "incoming_allow": incoming_allowed,
+                "outgoing_allow": outgoing_allowed
+                    }
+                },
+                "app": app
             })
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic TUFNSk01TlRCS01NSkhPVFJNWkQ6TkRBeU5HTTFZelkwTTJVeFkyTmlNak16TVdGbU1qVXlNVGc0TmprNA=='
-        }
-        print("payload", payload)
-        print("headers", headers)
-        print("url", url)
-        response = requests.request("POST", url, headers=headers,data=payload)
-        return response.text
+            return self.client.request('POST', ('JWT/Token', ), to_param_dict(self.create, locals()), is_voice_request=True)
