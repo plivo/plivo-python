4d190fbccbb57d87eec60f235ff3e10411d0b349
kowshik-plivo <intern-kowshik@Plivo.com>
2022-07-17 00:29:56 +0530

    VT-4406

--- plivo/resources/token.py
+++ plivo/resources/token.py
@@ -6,32 +6,18 @@ from plivo.utils.validators import *
 class Token(PlivoResourceInterface):
     @validate_args(
         iss = [required(of_type(six.text_type))],
-        auth_token = [required(of_type(six.text_type))],
         sub = [optional(of_type(six.text_type))],
-        nbf = [required(of_type(six.text_type))],
-        exp = [required(of_type(six.text_type))],
+        nbf = [optional(of_type(six.text_type))],
+        exp = [optional(of_type(six.text_type))],
         incoming_allowed = [optional(of_type(bool))],
         outgoing_allowed = [optional(of_type(bool))],
         app = [optional(of_type(six.text_type))]
     )
 
-    def create( self,iss, auth_token, sub,nbf, exp, incoming_allowed, outgoing_allowed,app):
+    def create( self, iss, sub = None, nbf = None, exp = None, incoming_allowed = None, outgoing_allowed = None, app = None):    
         if(incoming_allowed == True and sub == None):
-                raise ValidationError('Sub is required')
+            raise ValidationError('Sub is required')
         elif(iss == None):
-                raise ValidationError('Iss is required')
+            raise ValidationError('Iss is required')
         else:
-            payload = json.dumps({
-                "sub": sub,
-                "iss": iss,
-                "nbf": nbf,
-                "exp": exp,
-                "per": {
-                "voice": {
-                "incoming_allow": incoming_allowed,
-                "outgoing_allow": outgoing_allowed
-                    }
-                },
-                "app": app
-            })
-            return self.client.request('POST', ('JWT/Token', ), to_param_dict(self.create, locals()), is_voice_request=True)
+            return self.client.request('POST', ('JWT','Token', ),to_param_dict(self.create,locals()), is_voice_request=True)      
