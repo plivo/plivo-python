7fdbd41dde89409fc807008c41ce08165a626976
kowshik-plivo <intern-kowshik@plivo.com>
2022-08-12 02:17:49 +0530

    Added unit tests

--- tests/resources/test_token.py
+++ tests/resources/test_token.py
@@ -6,7 +6,7 @@ from tests.decorators import with_response
 class TokenTest(PlivoResourceTestCase):
     @with_response(200)
     def test_create(self):
-        self.client.token.create("MAMDVLZJY2ZGY5MWU1ZJ")
+        self.client.token.create("MAXXXXXXXXXXXXXXXXXX")
         self.assertEqual(self.client.current_request.method, 'POST')
         self.assertUrlEqual(
             self.get_voice_url('JWT/Token'), self.client.current_request.url)
