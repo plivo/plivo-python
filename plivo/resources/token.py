9a9ee60658c0e71db1c7e144a3f3753785a5b348
kowshik-plivo <intern-kowshik@plivo.com>
2022-08-11 23:08:35 +0530

    Added unit tests

--- plivo/resources/token.py
+++ plivo/resources/token.py
@@ -1,42 +1,39 @@
-import json
 import string
-from plivo.base import (ListResponseObject, PlivoResource,PlivoResourceInterface)
-from plivo.utils import to_param_dict
+
+from plivo.base import (PlivoResourceInterface)
 from plivo.utils.validators import *
+
 
 class Token(PlivoResourceInterface):
     @validate_args(
-        iss = [required(of_type(six.text_type))],
-        sub = [optional(of_type(six.text_type))],
-        nbf = [optional(of_type(six.text_type))],
-        exp = [optional(of_type(six.text_type))],
-        incoming_allow = [optional(of_type(bool,string))],
-        outgoing_allow = [optional(of_type(bool,string))],
-        app = [optional(of_type(six.text_type))]
+        iss=[required(of_type(six.text_type))],
+        sub=[optional(of_type(six.text_type))],
+        nbf=[optional(of_type(six.text_type))],
+        exp=[optional(of_type(six.text_type))],
+        incoming_allow=[optional(of_type(bool, string))],
+        outgoing_allow=[optional(of_type(bool, string))],
+        app=[optional(of_type(six.text_type))]
     )
-
-    def create( self, iss, sub = None, nbf = None, exp = None, incoming_allow = None, outgoing_allow = None, app = None):    
-        if(incoming_allow == True and sub == None):
-            raise ValidationError('Sub is required')
-        elif(iss == None):
-            raise ValidationError('Iss is required')
+    def create(self, iss, sub=None, nbf=None, exp=None, incoming_allow=None, outgoing_allow=None, app=None):
+        if incoming_allow is True and sub is None:
+            raise ValueError('sub is required when incoming_allow is true')
         else:
-            params = {}
-            params['iss'] = iss
-            
-            if sub != None:
+            params = {'iss': iss}
+
+            if sub:
                 params['sub'] = sub
-            if nbf != None:
+            if nbf:
                 params['nbf'] = nbf
-            if exp != None:
+            if exp:
                 params['exp'] = exp
-            if incoming_allow != None or outgoing_allow != None:
+            if incoming_allow or outgoing_allow:
                 params['per'] = {}
                 params['per']['voice'] = {}
-                if incoming_allow != None:
+                if incoming_allow:
                     params['per']['voice']['incoming_allow'] = incoming_allow
-                if outgoing_allow != None:
+                if outgoing_allow:
                     params['per']['voice']['outgoing_allow'] = outgoing_allow
-            if app != None:
+            if app:
                 params['app'] = app
-            return self.client.request('POST', ('JWT','Token', ),params, is_voice_request=True)      
+
+            return self.client.request('POST', ('JWT', 'Token',), params, is_voice_request=True)
---
+++ tests/resources/test_token.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from tests import PlivoResourceTestCase
+from tests.decorators import with_response
+
+
+class TokenTest(PlivoResourceTestCase):
+    @with_response(200)
+    def test_create(self):
+        self.client.endpoints.create("MAMDVLZJY2ZGY5MWU1ZJ")
+        self.assertEqual(self.client.current_request.method, 'POST')
+        self.assertUrlEqual(
+            self.get_voice_url('JWT/Token'), self.client.current_request.url)
