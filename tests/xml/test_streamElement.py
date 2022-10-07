e29e1c306e055ce80641043d29353c45a2a3fe08
anindya.mahajan <anindya.mahajan@plivo.com>
2022-10-07 13:35:22 +0530

    Fixed XML class initializer for stream element and modified its test case to check for the fixed logic

--- tests/xml/test_streamElement.py
+++ tests/xml/test_streamElement.py
@@ -6,15 +6,16 @@ from tests import PlivoXmlTestCase
 
 class StreamElementTest(TestCase, PlivoXmlTestCase):
     def test_set_methods(self):
-        expected_response = '<Response>' \
-                            '<Stream bidirectional="true">wss://test.url</Stream>' \
-                            '</Response>'
+        expected_response = """<Response>
+                                    <Stream bidirectional="true" extraHeaders="{'abX-PH': 'cd'}">wss://test.url</Stream>
+                                </Response>"""
 
         content = 'wss://test.url'
         bidirectional = True
+        extraHeaders = {'ab': 'cd'}
 
         element = plivoxml.ResponseElement()
         response = element.add(
-            plivoxml.StreamElement(content, bidirectional=bidirectional)
+            plivoxml.StreamElement(content, bidirectional=bidirectional, extraHeaders=extraHeaders)
         ).to_string(False)
         self.assertXmlEqual(response, expected_response)
