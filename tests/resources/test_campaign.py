0d7f525f89c7e9a972043cfc3bbe48ed2050489b
NirmitiJain <nirmiti.jain@plivo.com>
2022-11-29 20:27:00 +0530

    UT

--- tests/resources/test_campaign.py
+++ tests/resources/test_campaign.py
@@ -66,7 +66,7 @@ class CampaignTest(PlivoResourceTestCase):
         response = self.client.campaign.update('CXNSG9W', sample1 = "sample message 1 needs minimum 20 character")
         self.assertEqual('PUT', self.client.current_request.method)
         self.assertUrlEqual(
-            'https://api.plivo.com/v1/Account/MAXXXXXXXXXXXXXXXXXX/10dlc/Campaign/',
+            'https://api.plivo.com/v1/Account/MAXXXXXXXXXXXXXXXXXX/10dlc/Campaign/CXNSG9W/',
             self.client.current_request.url)
 
     @with_response(200)
