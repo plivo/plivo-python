7f0bea5dcda9b32144ef477d6eea7a84384f0fc8
narayana <narayana@plivo.com>
2023-01-25 22:46:59 +0530

    version upgrade

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Communications :: Telephony',
     ],
