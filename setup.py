6e06f34b653fcf5321e35f67a9f688ab03dd737a
kalyan-plivo <kalyan.sunku@plivo.com>
2023-05-04 10:48:49 +0530

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ See https://github.com/plivo/plivo-python for more information.
 
 setup(
     name='plivo',
-    version='4.33.1',
+    version='4.34.0',
     description='A Python SDK to make voice calls & send SMS using Plivo and to generate Plivo XML',
     long_description=long_description,
     url='https://github.com/plivo/plivo-python',
