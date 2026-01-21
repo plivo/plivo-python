4a6808168fa7e6940228c0c94f31e25957a356c6
narayana-plivo <narayana@plivo.com>
2026-01-21 08:03:56 +0530

    ha number

--- tests/resources/test_numbers.py
+++ tests/resources/test_numbers.py
@@ -79,3 +79,10 @@ class PhoneNumberTest(PlivoResourceTestCase):
         self.assertUrlEqual(self.client.current_request.url,
                             self.get_url('PhoneNumber', number_id))
         self.assertEqual(self.client.current_request.method, 'POST')
+
+    @with_response(202)
+    def test_create_with_ha_enabled(self):
+        self.client.numbers.buy(number_id, app_id='test', ha_enabled=True)
+        self.assertUrlEqual(self.client.current_request.url,
+                            self.get_url('PhoneNumber', number_id))
+        self.assertEqual(self.client.current_request.method, 'POST')
